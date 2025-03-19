383744c6ae212be33359aece1cd41b5b9a27b59e
reyhanehxl <reyhaneh.amanollahii@gmail.com>
2025-03-19 12:30:42 +0330

    Set TransactionAccount

--- src/TransactionAccount.java
+++ src/TransactionAccount.java
@@ -1,3 +1,21 @@
 public class TransactionAccount extends BankAccount{
+    private final double overDraftLimit = 500;
+    public TransactionAccount(String accountHolderName, String accountNumber, double initialBalance){
+        super(accountHolderName, accountNumber, initialBalance);
+    }
+    @Override
+    public void calculateInterest(){
+        System.out.println("This account have no interests!");
+    }
 
+    @Override
+    public void withdraw(double amount) {
+        if(balance + overDraftLimit - amount > 0){
+            balance -= amount;
+            System.out.println(amount + " withdrawn. balance : " + balance);
+        }
+        else{
+            System.out.println("withdrawn denied.");
+        }
+    }
 }
